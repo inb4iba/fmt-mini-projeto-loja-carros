e4b15ec2fb7d5c511dd0c1aa2d7d70e93b89a7d4
Icaro <iba1987@gmail.com>
2023-02-15 20:33:39 -0300

    exercise 3

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const carBrands = [];
 const carModels = [];
+const cars = [];
 
 function registerBrand(brand) {
   carBrands.push(brand);
@@ -11,10 +12,29 @@ function registerModel(model, brand) {
   console.log(`Modelo ${model} cadastrada com sucesso!`);
 }
 
+function registerCar(model, brand, km, value, color) {
+  cars.push({
+    model,
+    brand,
+    km,
+    value,
+    color,
+  });
+  console.log("Veículo cadastrado com sucesso!");
+}
+
 for (let i = 1; i <= 3; i++) {
   registerBrand(`Marca ${i}`);
   registerModel(`Modelo ${i}`, `Marca ${Math.round(i / 2)}`);
+  registerCar(
+    `Modelo ${i}`,
+    `Marca ${Math.round(i / 2)}`,
+    20 * i,
+    50000 - i * 10000,
+    "preto"
+  );
 }
 
 console.log(carBrands);
 console.log(carModels);
+console.log(cars);
