d2aaaddb14ea0012b95af5d4e2137b77fad164f0
Icaro <iba1987@gmail.com>
2023-02-21 18:42:40 -0300

    fix home grid

--- scripts/script.js
+++ scripts/script.js
@@ -6,26 +6,23 @@ function displayVehicles() {
   const vehicles = getAllVehicles();
 
   for (const vehicle of vehicles) {
-    document.querySelector(".container").appendChild(createVehicled(vehicle));
+    document.querySelector(".container").appendChild(createCard(vehicle));
   }
 }
 
-function createVehicled(vehicle) {
-  const vehicled = document.createElement("div");
-  vehicled.classList = "vehicled";
-  vehicled.innerHTML = `
-    <img
-      src="https://images.unsplash.com/photo-1533473359331-0135ef1b58bf?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=270&q=80"
-      alt=""
-    />
-    <p class="model">${vehicle.model} - <span class="brand">${vehicle.brand}</span></p>
+function createCard(vehicle) {
+  const card = document.createElement("div");
+  card.classList = "card";
+  card.innerHTML = `
+    <img src="${vehicle.image}"/>
+    <p class="model">${vehicle.model.name} - <span class="brand">${vehicle.model.brand}</span></p>
     <p class="color">${vehicle.color}</p>
     <div class="flex mt">
       <p class="km">${vehicle.km}km</p>
       <p class="value">R$${vehicle.value}</p>
     </div>
   `;
-  return vehicled;
+  return card;
 }
 
 displayVehicles();
